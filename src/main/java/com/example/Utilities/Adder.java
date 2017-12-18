2479ea2a75738d90a8353d8afacc5a7182b79b45
Dave Engle <davengle@gmail.com>
2017-12-18 01:57:05 -0500

    added Adder class code and failing unit test

---
+++ src/main/java/com/example/Utilities/Adder.java
@@ -0,0 +1,9 @@
+package com.example.Utilities;
+
+public class Adder {
+
+
+    public int sum2Nums(int first, int second) {
+        return first + second;
+    }
+}
---
+++ src/test/java/com/example/Utilities/AdderTest.java
@@ -0,0 +1,18 @@
+package com.example.Utilities;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+public class AdderTest {
+
+    @Test
+    public void sum2Nums() {
+        Adder adder = new Adder();
+        int sum = adder.sum2Nums(2,3);
+        assertEquals(4, sum);
+    }
+}
