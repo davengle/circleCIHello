eb60f4e143b32b5ae8d1b19cfb6a2a220b866d73
Dave Engle <davengle@gmail.com>
2017-12-18 01:06:09 -0500

    first commit

---
+++ src/main/java/com/example/circlecihello/CircleCiHelloApplication.java
@@ -0,0 +1,16 @@
+package com.example.circlecihello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleCiHelloApplication.class, args);
+
+		System.out.println("############");
+		System.out.println("Hello World");
+		System.out.println("############");
+	}
+}
